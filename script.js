943a94c6c2180df370e865dc412f7c5be5b35746
Kanagarajkj <kanagarajkj98@gmail.com>
2022-07-03 00:49:59 +0530

    Basics Completed

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+
+// Image Slider
+let counter = 1;
+setInterval(() => {
+  document.getElementById('radio' + counter).checked = true;
+  counter++;
+
+  if (counter > 4) {
+    counter = 1;
+  }
+}, 3000);
